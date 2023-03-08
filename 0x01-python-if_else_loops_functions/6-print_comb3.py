060daf3bfe695cfe0aacd7f679f36184e326817c
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-07 22:13:08 -0800

    update task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
-    for j in range(1, 10):
+    for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print('{:d}{:d}'.format(i, j))
         print("{}{}".format(i, j), end=', ')
