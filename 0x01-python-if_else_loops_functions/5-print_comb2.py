104adeb0689268846af77910be063f539c6bfd2f
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-07 21:42:08 -0800

    update task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 10:
-        print("0{}".format(i), end=', ')
+    if n != 99:
+        print('{:02d}'.format(n), end=', ')
     else:
-        print(i, end=', ')
+        print('{:02d}'.format(n))
