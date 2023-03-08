62077ea644bf6b92dc4b784c16594f10c6b98503
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-07 20:53:23 -0800

    update task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new_str = ''
     for i in str:
         j = ord(i)
         if 96 < j < 123:
             j -= 32
-        new_str = chr(j)
-        print('{}'.format(new_str), end='')
+        new_str += chr(j)
+    print('{}'.format(new_str + ' '))
