92ff1a75bcfdef4da5cc8c56c8bdf2820fc7299c
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-09 07:35:12 -0800

    update task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
 
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
 
-print("{} arguments.".format(num_arguments))
-for arg in arguments:
-    print("{}: {}".format((arguments.index(arg) + 1), arg))
+    print("{} arguments.".format(num_arguments))
+    for arg in arguments:
+        print("{}: {}".format((arguments.index(arg) + 1), arg))
