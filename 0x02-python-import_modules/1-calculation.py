a0f7da75d4a6997162309bcd7ddcf23c5323f834
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-09 07:24:58 -0800

    update task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("a + b = {}".format(a, b, add(10, 5)))
-print("a - b = {}".format(a, b, sub(10, 5)))
-print("a * b = {}".format(a, b, mul(10, 5)))
-print("a / b = {}".format(a, b, div(10, 5)))
+print("{} + {} = {}".format(a, b, add(10, 5)))
+print("{} - {} = {}".format(a, b, sub(10, 5)))
+print("{} * {} = {}".format(a, b, mul(10, 5)))
+print("{} / {} = {}".format(a, b, div(10, 5)))
