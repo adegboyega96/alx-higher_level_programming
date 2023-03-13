4323796c0cbb90c2b74ab4dc172c45c2aa54678a
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-12 22:01:33 -0700

    update task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        # print(i)
+        for j in i:
+            print(j, end=' ')
+        print()
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    max_val = 0
+    for i in my_list:
+        if i > max_val:
+            max_val = i
+    return max_val
