4f5f661c40b93548d90342743e63693a28c5c3e1
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-13 21:22:30 -0700

    update task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 and len(tuple_b) < 2:
-        total = (tuple_a[0] + tuple_b[0], 0)
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
 
-    elif len(tuple_a) == 0 and len(tuple_b) == 0:
-        total = ()
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
 
-    elif len(tuple_a) >= 2 and len(tuple_b) == 0:
-        total = (tuple_a[0] + 0, tuple_a[1] + 0)
-
-    elif len(tuple_a) == 2 and len(tuple_b) >= 2:
-        total = (0 + tuple_b[0], 0 + tuple_b[1])
-
-    elif len(tuple_a) < 2 and len(tuple_b) >= 2:
-        total = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-
-    elif len(tuple_a) >= 2 and len(tuple_b) < 2:
-        total = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-    else:
-        total = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return total
+    added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return added_tuple
