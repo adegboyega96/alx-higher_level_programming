f4162a5d0c04e42932ed858e8bc85b8b5e9f1315
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-15 21:59:46 -0700

    update task

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        a_dictionary[i] *= 2
-    return a_dictionary
+    return {key: val * 2 for key, val in a_dictionary.items()}
