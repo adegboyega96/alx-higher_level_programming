0fb549f4cc62fef86f96d35662fe248231810ce1
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-29 09:16:22 -0700

    update task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,11 +9,11 @@ class Square:
         """initialize a square"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        
+        self.__size = size
 
     def area(self):
-        """Calculate area of the square
+        """Calculate area of the square"""
         return self.__size ** 2
