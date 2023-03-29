7bcf9efece57b86781069b99060b70b6b2137c32
Emmanuel Adetoro <bizemmang@gmail.com>
2023-03-29 08:51:07 -0700

    update task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Define a square"""
 
 class Square:
+    """Representing the square"""
 
     def __init__(self, size):
+        """initializing the square"""
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-
+"""Defines a square """
 class Square:
+    """Represents a square """
 
     def __init__(self, size=0):
+        """initialize a square """
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
