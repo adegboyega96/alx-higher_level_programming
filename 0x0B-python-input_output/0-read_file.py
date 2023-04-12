4470e3af6eb4f8529d7d21ecab420e8f18d3ab4d
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-12 09:15:04 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Text reading module from a file"""
+
 
 def read_file(filename=""):
+    """Prints the contents insde the text file"""
     with open(filename, encoding="utf-8") as data:
-        read_data = data.read()
-    print(read_data)
+        print(data.read())
