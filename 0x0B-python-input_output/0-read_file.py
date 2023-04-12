0ced45911d2f8b0846068ab961e473375be24a24
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-12 09:06:57 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as data:
         read_data = data.read()
-        print(read_data)
+    print(read_data)
