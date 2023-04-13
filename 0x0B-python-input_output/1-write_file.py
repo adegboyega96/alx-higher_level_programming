5de7712a5d0ee7f07669d48d4723a2e5099ac6d9
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-12 21:00:41 -0700

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module write a function that writes a string to a text file (UTF8) and returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, 'w') as f:
+        return f.write(text)
