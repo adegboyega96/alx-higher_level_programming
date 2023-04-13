000260df3c5d8d3069f9bb06609776f1b39eb08a
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-12 21:54:50 -0700

    update task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module adds all argumes to a python list
+and then save to a file """
+import sys
+
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+
+    filename = "add_item.json"
+    try:
+        lst = load_from_json_file(filename)
+        for arg in sys.argv[1:]:
+            lst.append(arg)
+        save_to_json_file(lst, filename)
+    except FileNotFoundError:
+        lst = []
+        for arg in sys.argv[1:]:
+            lst.append(arg)
+        save_to_json_file(lst, filename)
