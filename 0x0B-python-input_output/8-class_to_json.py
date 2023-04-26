c307a4a707c1bdde16e2da6b4eb5d14b8e8c8604
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-26 07:26:10 -0700

    update task

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -3,5 +3,6 @@
 
 
 def class_to_json(obj):
-    """function that returns the dictionary description with simple data structure"""
+    """function that returns the dictionary description with
+    simple data structure"""
     return obj.__dict__
