2acace7d7ac4edc209194aa80ef35fbedea683b3
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-26 07:04:52 -0700

    update task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""This module adds all argumes to a python list
-and then save to a file """
+"""This module adds all arguments to a python list
+and then save to a json file """
 
 
 import sys
+# load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+# save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-if __name__ == "__main__":
-    load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
-
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+data.extend(sys.argv[1:])
+save_to_json_file(data, "add_item.json")
