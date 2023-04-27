5b6f614209b236acd0b6eb1f6dcb51af7c018e8d
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-26 19:58:37 -0700

    update task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -20,6 +20,5 @@ class Student:
 
     def reload_from_json(self, json):
         """replaces all attributes of the Student instance"""
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        for k, v in json.items():
+            setattr(self, k, v)
