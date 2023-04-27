7c4e3831adb968a4ebc7ed7adf547f73382a8bcc
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-26 19:42:48 -0700

    update task

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Student module"""
+
+
+class Student:
+    """Student class module"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is not None:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
