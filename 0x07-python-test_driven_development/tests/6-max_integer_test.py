98d729556e7c04e621fb4145f49e52ddac67f5c8
Emmanuel Adetoro <bizemmang@gmail.com>
2023-04-30 17:50:33 -0700

    update task

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -45,12 +45,12 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_string(self):
         """Test a string."""
-        string = "Ehoneah"
-        self.assertEqual(max_integer(string), 'r')
+        string = "Adetoro"
+        self.assertEqual(max_integer(string), 't')
 
     def test_list_of_strings(self):
         """Test a list of strings."""
-        strings = ["Ehoneah", "is", "my", "name"]
+        strings = ["Adetoro", "is", "my", "name"]
         self.assertEqual(max_integer(strings), "name")
 
     def test_empty_string(self):
