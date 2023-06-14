1b4d4e6fc1e83c60309ffbf5719d263bc2eaa226
Emmanuel Adetoro <emmanueladetoro72@gmail.com>
2023-06-14 04:10:46 +0000

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if (process.argv.length > 2)
+{
+    if (isNaN(process.argv[2]))
+    {
+        console.log("Not a number");
+    }
+    else
+    {
+        console.log("My number: " + Math.floor(process.argv[2]));
+    }
+}
