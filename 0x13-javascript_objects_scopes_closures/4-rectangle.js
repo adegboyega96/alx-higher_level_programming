64df76f000b458dbf52fdc2d0a57762ce990a8f2
Emmanuel Adetoro <emmanueladetoro72@gmail.com>
2023-06-15 04:13:36 +0000

    update tasks

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return Object.create(Rectangle.prototype);
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    let rows = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rows += 'X'
+      }
+      if (i < this.height - 1) rows += "\n";
+    }
+    console.log(rows);
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2
+  }
+}
+
+module.exports = Rectangle;
